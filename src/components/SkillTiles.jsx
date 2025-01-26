fccc7c3c9a5a3677cef39eb41f76b9fb3cb8931d
Mriganka Paul <ironmaniscool99@gmail.com>
2025-01-27 00:38:00 +0530

    minor change

--- src/components/SkillTiles.jsx
+++ src/components/SkillTiles.jsx
@@ -1,6 +1,6 @@
 function SkillTiles() {
     const skills = ["c++", "javascript", "python", "c", "java", "html", "css", "tailwindcss",
-        "react", "nodejs", "mongodb", "mysql", "git", "postman", "microsoft windows",
+        "react", "nodejs", "mongodb", "mysql", "git", "postman", "nextjs","windows",
         "linux", "visual studio code", "apache netbeans", "figma", "adobe photoshop", "canva"
     ];
     // const progLang = ["c++", "javascript", "python", "c", "java",];
@@ -18,7 +18,7 @@ function SkillTiles() {
                     key={skill}
                     className="border-2 border-link-border px-3 py-1 rounded-full 
                     min-w-12 flex justify-center items-center outline outline-0 outline-link-border
-                    hover:outline-[3.7px] transition-all duration-200 cursor-pointer"
+                    hover:outline-[4px] transition-all duration-200 cursor-pointer"
                 >
                     <span>{skill}</span>
                 </li>
